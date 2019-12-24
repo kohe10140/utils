[fix]The calculation speed has been improved.

ml/iter_recommender.py
@@ -122,6 +122,7 @@ class Iterative:
         ------
         ValueError
         """
+        add_index = add_index.astype(int)
         if stacking == 'v':
             updated_exist = np.vstack([exist_data, exploration_data[add_index]]) 
             updated_exploration = np.delete(exploration_data, add_index, axis=0)
@@ -153,15 +154,17 @@ class Iterative:
         self.n_initial = n_initial
 
         if initial_indexes == 'random':
-            initial_indexes = np.random.choice(np.arange(len(self.y)), self.n_initial)
+            self.initial_indexes = np.random.choice(np.arange(int(len(self.y)/2)),
+                                                    self.n_initial,
+                                                    replace=False)
 
-        self.train_X = self.X[initial_indexes]
-        self.train_y = self.y[initial_indexes]
+        self.train_X = self.X[self.initial_indexes]
+        self.train_y = self.y[self.initial_indexes]
 
-        ind = np.ones(len(self.y), dtype=bool)
-        ind[initial_indexes] = False
-        self.exploration_X = self.X[ind]
-        self.exploration_y = self.y[ind]
+        #ind = np.ones(len(self.y), dtype=bool)
+        #ind[self.initial_indexes] = False
+        self.exploration_X = np.delete(self.X, self.initial_indexes, axis=0)#self.X[ind]
+        self.exploration_y = np.delete(self.y, self.initial_indexes, axis=0)#self.y[ind]
 
         return self.train_X, self.train_y, self.exploration_X, self.exploration_y
 
@@ -175,9 +178,10 @@ class Iterative:
         return index 
 
 
+## EXAMPLE OF ITERATIVE RECOMENDATION ####################
     def iter_recommend(self, n_initial, random_state,
                        param_grid, save_path, top_n,
-                       initial_indexes='random',
+                       initial_indexes='random', n_jobs=-1,
                        eval_criteria=False, retune=False):
         """
         Recommend and output the result

ml/tensor_utils.py
@@ -20,10 +20,10 @@ def ratio_sym(r1, r2, sep=':'):
     >>> ratio_sym('2:1', '1:2')
     True
     """
-    r1_set = set(r1.split(sep=sep))
-    r2_set = set(r2.split(sep=sep))
+    r1_list = r1.split(sep=sep)
+    r2_list = r2.split(sep=sep)
 
-    if list(r1_set) == list(r2_set):
+    if (r1_list[0]==r2_list[1]) & (r1_list[1]==r2_list[0]):
         return True
     else:
         return False
@@ -47,7 +47,6 @@ def check_double(entry1, entry2):
     >>> check_double(entry1, entry2)
     True
     """
-    #import pdb; pdb.set_trace()
     sym = entry1[0]==entry2[1] and \
           entry1[1]==entry2[0] and \
           entry1[2]==entry2[2] and \
@@ -61,7 +60,37 @@ def check_double(entry1, entry2):
         return False
 
 
-def identical2mean(X, y):
+def permutate(li):
+    """
+    Have the list permutated
+    Parameters
+    ----------
+    li : array-like
+        The array to be arranged alphabetically
+    
+    Returns
+    -------
+    array-like
+        The array to be arranged alphabetically
+
+    Example
+    -------
+    >>> permutate(['B', 'A', 700, '1:2'])
+    ['A', 'B', 700, '2:1']
+    """
+    if li[0] > li[1]:
+        esc = li[0]
+        li[0] = li[1]
+        li[1] = esc
+        ratio = li[-1].split(':')
+        esc = ratio[0]
+        ratio[0] = ratio[1]
+        ratio[1] = esc
+        li[-1] = ratio[0] + ':' + ratio[1]
+    return li
+
+
+def identical2mean(str_data, y):
     """
     Parameter
     ---------
@@ -79,23 +108,22 @@ def identical2mean(X, y):
     i_mean : numpy array
         pred to be taken mean in the identical data
     """
-    for entry in X:
-        temp_index = []
-        for i in range(len(X)):
-            if check_double(entry, X[i]):
-                temp_index.append(i)
-        y[temp_index] = np.mean(y[temp_index])
-
-    i_mean = y
-    return i_mean
+    # to take mean of identical entries of y
+    i_mean_y = np.zeros(len(y))
+    for i in range(len(y)):
+        index = np.where(str_data==str_data[i])
+        print(index)
+        i_mean_y[index] = y[index].mean()
+    return i_mean_y
 
 
-def unique(data, pred, criteria=check_double):
+def unique(str_data, pred, criteria=check_double):
     """
     Parameter
     ---------
-    data : array-like (n_samples, n_features)
+    str_data : array-like of string (n_samples)
         The array to be unique
+        ex) ['A_B_500_1:2', 'C_D_700_2:3']
 
     pred : array-like of shape (n_predictions)
         The array of the predictions
@@ -107,52 +135,49 @@ def unique(data, pred, criteria=check_double):
     ------
     unique_data : numpy array
     """
-    unique_X = data[0][np.newaxis, :]
-    unique_y = pred[0]
-    for i in range(len(data)):
-        for j in unique_X:
-            if check_double(data[i], j):
-                break
-        else:
-            unique_X = np.vstack([unique_X, data[i]])
-            unique_y = np.hstack([unique_y, pred[i]])
+    unique_X, index = np.unique(str_data, return_index=True)
+    unique_y = pred[index]
     return unique_X, unique_y
 
 
-def get_top(X, y, top_n):
+def get_top(str_data, uni_X, uni_y, top_n):
     """
     Parameter
     ---------
-    X : array-like of shape (n_samples, n_features)
-        The unique array 
+    str_data : array-like of string (n_samples)
+        The string data like below
+        ex) ['A_B_500_1:2', 'C_D_700_2:3']
 
-    y : array-like of shape (n_predictions)
+    uni_X : array-like of string (n_samples(unique))
+        The unique array 
+    
+    uni_y : array-like of shape (n_predictions)
         The unique array of the predictions
 
     top_n : int
         Top nth score in all the predictions
+
     Return
     ------
     numpy array
-        The top nth index considered symmetory
+        The top nth index considered symmetory,
+        so the size of return array will be larger than n.
     """
-    if top_n < len(X):
-        index = np.argpartition(-y, top_n)[:top_n]
+    if top_n < len(uni_X):
+        index = np.argpartition(-uni_y, top_n)[:top_n]
     else:
-        index = np.arange(len(X))
+        index = np.arange(len(uni_X))
+    top_entries = uni_X[index]
+    top_n_index = np.array([])
+    for entry in top_entries:
+        top_n_index = np.hstack([top_n_index, np.where(str_data==entry)[0]])
 
-    top_n_index = []
-    for entry in X[index]:
-        for i in range(len(X)):
-            if check_double(entry, X[i]):
-                top_n_index.append(i)
-
-    return np.array(top_n_index)
+    return top_n_index
 
 
 def sym_mean(y, top_n, X):
-    i_mean_y = identical2mean(X, y)
-    unique_X, unique_y = unique(X, i_mean_y)
-    top_n_index = get_top(unique_X, unique_y, top_n)
+    str_data = np.array(['_'.join(permutate(entry).astype(str)) for entry in X])
+    i_mean_y = identical2mean(str_data, y)
+    unique_X, unique_y = unique(str_data, i_mean_y)
+    top_n_index = get_top(str_data, unique_X, unique_y, top_n)
     return top_n_index
-

test/test_tensor_utils.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from ml.tensor_utils import identical2mean, unique, get_top, sym_mean
+from ml.tensor_utils import identical2mean, unique, get_top, sym_mean, permutate
 
 
 X = np.array([['A', 'B', 500, '1:2'], #0
@@ -21,12 +21,14 @@ X = np.array([['A', 'B', 500, '1:2'], #0
 y = np.arange(len(X)).astype(float)
 y_ = np.arange(len(X)).astype(float)
 y__ = np.arange(len(X)).astype(float)
+str_data = np.array(['_'.join(permutate(entry).astype(str)) for entry in X])
 
 
 class TestTensorUtils(unittest.TestCase):
 
     def test_identical2mean(self):
-        a_i_mean = identical2mean(X=X, y=y)
+        str_data = np.array(['_'.join(permutate(entry).astype(str)) for entry in X])
+        a_i_mean = identical2mean(str_data=str_data, y=y)
         e_i_mean = np.array([1.0, 1.0, 1.0, 
                              3.5, 3.5, 5.0,
                              7.0, 7.0, 7.0,
@@ -35,31 +37,32 @@ class TestTensorUtils(unittest.TestCase):
 
 
     def test_unique(self):
-        a_unique_X, a_unique_y = unique(data=X, pred=y_)
-        e_unique_X = np.array([['A', 'B', 500, '1:2'], 
-                               ['A', 'B', 500, '1:1'], 
-                               ['A', 'B', 300, '1:2'], 
-                               ['A', 'C', 500, '1:2'], 
-                               ['A', 'C', 500, '1:1'], 
-                               ['A', 'C', 300, '1:2']])
-        e_unique_y = np.array([0, 3, 5, 6, 9, 11])
+        str_data = np.array(['_'.join(permutate(entry).astype(str)) for entry in X])
+        a_unique_X, a_unique_y = unique(str_data=str_data, pred=y_)
+        e_unique_X = np.array(['A_B_300_1:2', 
+                               'A_B_500_1:1', 
+                               'A_B_500_1:2', 
+                               'A_C_300_1:2', 
+                               'A_C_500_1:1', 
+                               'A_C_500_1:2'])
+        e_unique_y = np.array([5, 3, 0, 11, 9, 6])
 
         np.testing.assert_array_equal(a_unique_X, e_unique_X)
         np.testing.assert_array_equal(a_unique_y, e_unique_y)
 
 
     def test_get_top(self):
-        unique_X, unique_y = unique(data=X, pred=y_)
-        e_top = get_top(unique_X, unique_y, 3)
-        a_top = np.array([3, 5, 4])
+        unique_X, unique_y = unique(str_data=str_data, pred=y_)
+        a_top = get_top(str_data, unique_X, unique_y, 3)
+        e_top = np.array([11, 6, 7, 8, 9, 10])
         np.testing.assert_array_equal(a_top, e_top)
         
 
     def test_sym_mean(self):
-        i_mean_y__ = identical2mean(X, y__)
-        unique_X, unique_y = unique(data=X, pred=i_mean_y__)
-        e_top = get_top(unique_X, unique_y, 3)
-        a_top = sym_mean(y=unique_y, top_n=3, X=unique_X)
+        i_mean_y__ = identical2mean(str_data, y__)
+        unique_X, unique_y = unique(str_data=str_data ,pred=i_mean_y__)
+        a_top = get_top(str_data, unique_X, unique_y, 3)
+        e_top = sym_mean(y=y__, top_n=3, X=X)
         np.testing.assert_array_equal(a_top, e_top)
 
 