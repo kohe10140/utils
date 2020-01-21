3d7870f4efaadf55b8a5dd50f44fb6ae7897fa76
kohe10410 <nishiwest.k@gmail.com>
2020-01-21 15:11:13 +0900

    [todo]Test and debug

--- ml/iter_recommender.py
+++ ml/iter_recommender.py
@@ -8,22 +8,23 @@ import pandas as pd
 
 class Iterative:
     
-    def __init__(self, estimator, X, y):
+    def __init__(self, estimator, df, y_col):
         """
         Parameters
         ----------
         estimator : estimator object
             This is assumed to implement the scikit-learn estimator interface. Either estimator needs to provide a score function, or scoring must be passed.
 
-        X : array-like of shape (n_samples, n_features)
-            Features
+        df : pandas.DataFrame
+            The dataframe that has features and target
 
-        y : array like of shape (n_samples)
-            Objective variables
+        y_col : str
+            The name of objective variables in df
         """
         self.estimator = estimator
-        self.X = X
-        self.y = y
+        self.df = df
+        self.df_X = df[df.columns[df.columns != y_col]]
+        self.df_y = df[y_col]
 
 
     def learning(self, param_grid, X, y, n_jobs=-1, verbose=3, cv=5):
@@ -41,7 +42,7 @@ class Iterative:
         return pred_X, pred_y
 
 
-    def get_top_index(self, top_n, pred, exploration_X, eval_criteria=False):
+    def get_top_index(self, top_n, df_pred, eval_criteria=False, sym='mean'):
         """
         To get the indexes of data whose top nth predictions 
         
@@ -50,12 +51,9 @@ class Iterative:
         top_n : int
             Top nth score in all the predictions
 
-        pred : array-like of shape (n_prediction)
+        df_pred : pandas.DataFrame 
             Predictions in the exploration space 
         
-        exploration_X : array-like of shape (n_samples, n_features)
-            features in the exploration space
-
         eval_criteria : function, optional
             The criteria to evaluate the predicted data, by default self._intact
             The argument of this function must be (y, top_n, X)
@@ -74,65 +72,62 @@ class Iterative:
             top_score_index : numpy array
                 The index of top nth score 
 
+        sym : int
+           The method to process symmetorical entries, by default 'mean'
+           'max', 'min'
+
         Returns
         -------
         numpy array of shape (n_top) 
             The indexes of data whose top nth predictions
         """
-        self.pred = pred
+        self.df_pred = df_pred
         self.top_n = top_n
         self.eval_criteria = eval_criteria
 
         if not eval_criteria:
             eval_criteria = self._intact
         
-        self.top_score_index = eval_criteria(pred, top_n, exploration_X)
+        if sym == 'mean':
+            self.df_pred = self.df_pred.groupby(df_pred.index).mean()
+        if sym == 'max':
+            self.df_pred = self.df_pred.groupby(df_pred.index).max()
+        if sym == 'min':
+            self.df_pred = self.df_pred.groupby(df_pred.index).min()
+        
+        self.top_score_index = eval_criteria(df_pred, top_n)
         return self.top_score_index
 
 
-    def parse_data(self, exist_data, add_index, exploration_data, stacking):
+    def parse_data(self, exist_index, add_index, exploration_index):
         """
         To add the recommended data to exist_data from exploration_data
         and delete recommended_data from exploration_data
 
         Parameters
         ----------
-        exist_data : numpy array of shape(n_samples, n_features) or (n_samples)  
-            The data to train
+        exist_index : array-like
+            The index of the train data
 
-        add_index : int or numpy array
+        add_index : array-like
             The index of exploration_data to be added to exist_data
 
-        exploration_data : numpy array of shape(n_samples, n_features) or (n_samples)
-            The data in exploration space
+        exploration_index : array-like
+            The index of the data in exploration space
 
-        stacking : str
-            'v' -> Stacking vertically 
-            's' -> Stacking horizontally
-        
         Returns
         -------
-        update_exist : numpy array
-            The exist data added recommendations
+        update_exist : array-like
+            The exist index of added recommendations
                 
-        update_exploration : numpy array
-            The exploration data without recommendations
+        update_exploration : array-like
+            The exploration index without recommendations
 
-        Raises
-        ------
-        ValueError
         """
         add_index = add_index.astype(int)
-        if stacking == 'v':
-            updated_exist = np.vstack([exist_data, exploration_data[add_index]]) 
-            updated_exploration = np.delete(exploration_data, add_index, axis=0)
-        elif stacking == 'h':
-            updated_exist = np.hstack([exist_data, exploration_data[add_index]]) 
-            updated_exploration = np.delete(exploration_data, add_index, axis=0)
-        else:
-            raise ValueError
-         
-        return updated_exist, updated_exploration
+        update_exist = np.unique(np.hstack([exist_index, add_index]))
+        update_exploration = np.unique(np.array([key for key in exploration_index if key not in add_index]))
+        return update_exist, update_exploration
 
 
     def initial_data(self, n_initial, initial_indexes='random', random_state=0):
@@ -147,41 +142,26 @@ class Iterative:
         
         Returns
         -------
-        The initial data of training(exist) data and exploration data
+        array-like
+            The initial index of training(exist) data and exploration data
         """
         np.random.seed(random_state)
         self.initial_indexes = initial_indexes
         self.n_initial = n_initial
 
         if initial_indexes == 'random':
-            self.initial_indexes = np.random.choice(np.arange(len(self.y)),
+            self.initial_indexes = np.random.choice(list(set(self.df_y.index)),
                                                     self.n_initial,
                                                     replace=False)
-        elif initial_indexes == 'sym_random':
-            self.initial_indexes = np.random.choice(np.arange(int(len(self.y)/2)),
-                                                    self.n_initial,
-                                                    replace=False)
-            sym_indexes = self.initial_indexes + np.full(len(self.initial_indexes), int(len(self.y)/2))
-            self.initial_indexes = np.hstack([self.initial_indexes, sym_indexes]).astype(int)
         else:
             self.initial_indexes = initial_indexes
-
-        self.train_X = self.X[self.initial_indexes]
-        self.train_y = self.y[self.initial_indexes]
-
-        self.exploration_X = np.delete(self.X, self.initial_indexes, axis=0)#self.X[ind]
-        self.exploration_y = np.delete(self.y, self.initial_indexes, axis=0)#self.y[ind]
-
-        return self.train_X, self.train_y, self.exploration_X, self.exploration_y
+        return self.initial_indexes
 
 
-    def _intact(self, y, top_n, X):
-
-        if top_n < len(y):
-            index = np.argpartition(-y, top_n)[:top_n]
-        else:
-            index = np.arange(len(y))
-        return index 
+    def _intact(self, df_y, top_n):
+        df_rank = df_y.rank(ascending=False)
+        index = df_rank[df_rank['y']<=top_n].index
+        return index.values 
 
 
 ## EXAMPLE OF ITERATIVE RECOMENDATION ####################
@@ -202,29 +182,35 @@ class Iterative:
         estimators = []
 
         # Extract initial data
-        self.train_X, self.train_y, self.exploration_X, self.exploration_y = self.initial_data(n_initial, initial_indexes, random_state)
-        self.best_estimator = self.learning(param_grid, X=self.train_X, y=self.train_y)
+        self.train_index = self.initial_data(n_initial, initial_indexes, random_state)
+        self.train_x = self.df_X.loc[self.train_index].values
+        self.train_y = self.df_y.loc[self.train_index].values
+        self.best_estimator = self.learning(param_grid, x=self.train_x, y=self.train_y)
 
-        rec_output = os.path.join(save_path, 'train'+'0'+'.cmp')
-        df = pd.DataFrame(self.train_X)
-        df['y'] = self.train_y
+        rec_output = os.path.join(save_path, 'train'+'0'+'.csv')
+        df = self.df.loc[self.train_index]
         df.to_csv(rec_output)
 
-        for i in range(int(len(self.y)/top_n)):
+        for i in range(int(len(self.df_y)/top_n)):
             # Recommend iteratively
             print('')
             print(str(i)+'th Recommendation')
-            pred_y = self.best_estimator.predict(self.exploration_X)
-            self.top_score_index = self.get_top_index(top_n, pred_y, self.exploration_X, eval_criteria)
-            self.train_X, self.exploration_X = self.parse_data(self.train_X, self.top_score_index, self.exploration_X, stacking='v')
-            self.train_y, self.exploration_y = self.parse_data(self.train_y, self.top_score_index, self.exploration_y, stacking='h')
+            self.exploration_X = self.df_X.drop(self.train_index)
+            pred_y = self.best_estimator.predict(self.exploration_X.values)
+            df_pred_y = pd.DataFrame(pred_y, index=self.exploration_X.index, columns=['y'])
+            self.top_score_index = self.get_top_index(top_n, df_pred_y, eval_criteria)
+            self.train_index, self.exploration_index = self.parse_data(self.train_X.index,
+                                                                       self.top_score_index,
+                                                                       self.exploration_X.index)
 
             # Save the used estimator and recommended data
             estimators.append(self.best_estimator)
-            rec_output = os.path.join(save_path, 'train'+str(i+1)+'.cmp')
-            df = pd.DataFrame(self.train_X)
-            df['y'] = self.train_y
+            rec_output = os.path.join(save_path, 'train'+str(i+1)+'.csv')
+            df = pd.merge(self.df_X.loc[self.train_index], self.df_y.loc[self.train_index], left_index=True, right_index=True)
             df.to_csv(rec_output)
+
+            self.train_x = self.df_X.loc[self.train_index].values
+            self.train_y = self.df_y.loc[self.train_index].values
 
             if retune:
                 self.best_estimator = self.learning(param_grid, X=self.train_X, y=self.train_y)
